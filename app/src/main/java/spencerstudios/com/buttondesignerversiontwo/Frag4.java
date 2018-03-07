a21d0f2f4f7f41492fe57f1a885da44f1f4770d2
Dean Spencer <deanspencer007@gmail.com>
2018-03-07 11:30:13 +0000

    mash up

--- app/src/main/java/spencerstudios/com/buttondesignerversiontwo/Frag4.java
+++ app/src/main/java/spencerstudios/com/buttondesignerversiontwo/Frag4.java
@@ -15,12 +15,17 @@ import android.widget.SeekBar;
 import android.widget.Switch;
 import android.widget.TextView;
 
+import java.util.Locale;
+
 public class Frag4 extends Fragment implements SeekBar.OnSeekBarChangeListener {
 
     private View view;
     private LinearLayout radContainer, angelContainer;
     private RadioGroup radioGroupGradientType;
     private TextView angleTitle, tvAngelDegrees, tvCenXDeg, tvCenYDeg, tvRadDp;
+    private Switch useGradient, threeColors;
+    private RadioButton radLinear, radRadial;
+    private AppCompatSeekBar seekAngle, seekCenX, seekCenY, seekRad;
 
     @Nullable
     @Override
@@ -30,32 +35,7 @@ public class Frag4 extends Fragment implements SeekBar.OnSeekBarChangeListener {
 
             view = inflater.inflate(R.layout.frag_4, container, false);
 
-            radioGroupGradientType = view.findViewById(R.id.radio_gradient_type);
-
-            radContainer = view.findViewById(R.id.radial_container);
-            angelContainer = view.findViewById(R.id.ll_angle_container);
-
-            final RadioButton radRadial = view.findViewById(R.id.radio_radial);
-            RadioButton radLinear = view.findViewById(R.id.radio_linear);
-
-            angleTitle = view.findViewById(R.id.tv_angle_title);
-            tvAngelDegrees = view.findViewById(R.id.tv_angle_degrees);
-            tvCenXDeg = view.findViewById(R.id.tv_center_x_dp);
-            tvCenYDeg = view.findViewById(R.id.tv_center_y_dp);
-            tvRadDp = view.findViewById(R.id.tv_radius);
-
-            AppCompatSeekBar seekAngle = view.findViewById(R.id.seek_angle);
-            AppCompatSeekBar seekCenX = view.findViewById(R.id.seek_radial_center_x);
-            AppCompatSeekBar seekCenY = view.findViewById(R.id.seek_radial_center_y);
-            AppCompatSeekBar seekRad = view.findViewById(R.id.seek_radial_radius);
-
-            final Switch useGradient = view.findViewById(R.id.switch_use_gradient);
-            Switch threeColors = view.findViewById(R.id.switch_use_3_colors);
-
-            seekAngle.setOnSeekBarChangeListener(this);
-            seekCenX.setOnSeekBarChangeListener(this);
-            seekCenY.setOnSeekBarChangeListener(this);
-            seekRad.setOnSeekBarChangeListener(this);
+            initiateViews();
 
             radRadial.setChecked(Utils.getBooleanPrefs(getActivity(), "use_radial"));
 
@@ -63,6 +43,13 @@ public class Frag4 extends Fragment implements SeekBar.OnSeekBarChangeListener {
                 @Override
                 public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                     Utils.setBooleanPrefs(getActivity(), "use_radial", b);
+                    if (b) {
+                        angelContainer.setVisibility(View.GONE);
+                        radContainer.setVisibility(View.VISIBLE);
+                    } else {
+                        angelContainer.setVisibility(View.VISIBLE);
+                        radContainer.setVisibility(View.GONE);
+                    }
                 }
             });
 
@@ -72,35 +59,73 @@ public class Frag4 extends Fragment implements SeekBar.OnSeekBarChangeListener {
                 @Override
                 public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                     Utils.setBooleanPrefs(getActivity(), "use_gradient", b);
-                    if (b) {
-                        radioGroupGradientType.setVisibility(View.VISIBLE);
-                        if (Utils.getBooleanPrefs(getActivity(), "use_radial")) {
-                            angelContainer.setVisibility(View.GONE);
-                            radContainer.setVisibility(View.VISIBLE);
-                        } else {
-                            angelContainer.setVisibility(View.VISIBLE);
-                            radContainer.setVisibility(View.GONE);
-                        }
-                    } else {
-                        radioGroupGradientType.setVisibility(View.GONE);
-                    }
+                    configLayouts();
                 }
             });
 
+            configLayouts();
+            setValues();
         }
         return view;
     }
 
-    private void setLayouts() {
-
+    private void configLayouts() {
+        if (Utils.getBooleanPrefs(getActivity(), "use_gradient")) {
+            radioGroupGradientType.setVisibility(View.VISIBLE);
+            if (Utils.getBooleanPrefs(getActivity(), "use_radial")) {
+                radRadial.setChecked(true);
+                radContainer.setVisibility(View.VISIBLE);
+                angelContainer.setVisibility(View.GONE);
+            } else {
+                radLinear.setChecked(true);
+                radContainer.setVisibility(View.GONE);
+                angelContainer.setVisibility(View.VISIBLE);
+            }
+        } else {
+            angelContainer.setVisibility(View.GONE);
+            radContainer.setVisibility(View.GONE);
+            radioGroupGradientType.setVisibility(View.GONE);
+        }
     }
+
 
     @Override
     public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
         switch (seekBar.getId()) {
 
+            case R.id.seek_radial_center_x:
+                tvCenXDeg.setText(String.format(Locale.getDefault(), "%d%%", i));
+                Utils.setDimensionPrefs(getActivity(), "center_x", i);
+                break;
+
+            case R.id.seek_radial_center_y:
+                tvCenYDeg.setText(String.format(Locale.getDefault(), "%d%%", i));
+                Utils.setDimensionPrefs(getActivity(), "center_y", i);
+                break;
+
+            case R.id.seek_radial_radius:
+                tvRadDp.setText(String.format(Locale.getDefault(), "%d DP", i+45));
+                Utils.setDimensionPrefs(getActivity(), "radius", i);
+                break;
+
+            case R.id.seek_angle:
+                tvAngelDegrees.setText(String.format(Locale.getDefault(), "%d degress", i+45));
+                Utils.setDimensionPrefs(getActivity(), "angel", i + 45);
+
 
         }
+    }
+
+    private void setValues(){
+        seekCenX.setProgress(Utils.getDimensionPrefs(getActivity(), "center_x"));
+        tvCenXDeg.setText(String.format(Locale.getDefault(), "%d%%", Utils.getDimensionPrefs(getActivity(), "center_x")));
+
+        seekCenY.setProgress(Utils.getDimensionPrefs(getActivity(), "center_y"));
+        tvCenYDeg.setText(String.format(Locale.getDefault(), "%d%%", Utils.getDimensionPrefs(getActivity(), "center_y")));
+
+        seekRad.setProgress(Utils.getDimensionPrefs(getActivity(), "radius"));
+        tvRadDp.setText(String.format(Locale.getDefault(), "%d DP", Utils.getDimensionPrefs(getActivity(), "radius")));
+
     }
 
     @Override
@@ -111,5 +136,34 @@ public class Frag4 extends Fragment implements SeekBar.OnSeekBarChangeListener {
     @Override
     public void onStopTrackingTouch(SeekBar seekBar) {
 
+    }
+
+
+    private void initiateViews() {
+        radioGroupGradientType = view.findViewById(R.id.radio_gradient_type);
+        radContainer = view.findViewById(R.id.radial_container);
+        angelContainer = view.findViewById(R.id.ll_angle_container);
+
+        radRadial = view.findViewById(R.id.radio_radial);
+        radLinear = view.findViewById(R.id.radio_linear);
+
+        angleTitle = view.findViewById(R.id.tv_angle_title);
+        tvAngelDegrees = view.findViewById(R.id.tv_angle_degrees);
+        tvCenXDeg = view.findViewById(R.id.tv_center_x_dp);
+        tvCenYDeg = view.findViewById(R.id.tv_center_y_dp);
+        tvRadDp = view.findViewById(R.id.tv_radius);
+
+        seekAngle = view.findViewById(R.id.seek_angle);
+        seekCenX = view.findViewById(R.id.seek_radial_center_x);
+        seekCenY = view.findViewById(R.id.seek_radial_center_y);
+        seekRad = view.findViewById(R.id.seek_radial_radius);
+
+        useGradient = view.findViewById(R.id.switch_use_gradient);
+        threeColors = view.findViewById(R.id.switch_use_3_colors);
+
+        seekAngle.setOnSeekBarChangeListener(this);
+        seekCenX.setOnSeekBarChangeListener(this);
+        seekCenY.setOnSeekBarChangeListener(this);
+        seekRad.setOnSeekBarChangeListener(this);
     }
 }
