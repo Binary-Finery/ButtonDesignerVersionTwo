7f90bc2c68ab30f802f57ba2b951c74a5536b1cb
Dean Spencer <deanspencer007@gmail.com>
2018-03-09 20:24:34 +0000

    mash up

--- app/src/main/java/spencerstudios/com/buttondesignerversiontwo/MainActivity.java
+++ app/src/main/java/spencerstudios/com/buttondesignerversiontwo/MainActivity.java
@@ -14,20 +14,15 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.DisplayMetrics;
+import android.util.TypedValue;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
-    private DisplayMetrics displayMetrics;
-    private GradientDrawable drawable;
-    private Button btn;
-
+    private Button button;
     private SharedPreferences prefs;
-    private SharedPreferences.OnSharedPreferenceChangeListener onSharedPreferenceChangeListener;
-
-
-    private Context context = this;
+    private Context ctx = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,7 +33,7 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
 
         SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
-        btn = findViewById(R.id.button_preview);
+        button = findViewById(R.id.button_preview);
         ViewPager viewPager = findViewById(R.id.container);
         TabLayout tabLayout = findViewById(R.id.tabs);
 
@@ -49,42 +44,13 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
 
         prefs = PreferenceManager.getDefaultSharedPreferences(this);
         prefs.registerOnSharedPreferenceChangeListener(this);
+
+        applyButtonProperties();
     }
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String s) {
-
-        drawable = new GradientDrawable();
-
-        //set button corner radii...
-        if (Utils.getBooleanPrefs(context, "switch_corner")) {
-            drawable.setCornerRadii(new float[]{
-                    convertDpToPx(Utils.getDimensionPrefs(context, "corner_tl")),
-                    convertDpToPx(Utils.getDimensionPrefs(context, "corner_tl")),
-                    convertDpToPx(Utils.getDimensionPrefs(context, "corner_tr")),
-                    convertDpToPx(Utils.getDimensionPrefs(context, "corner_tr")),
-                    convertDpToPx(Utils.getDimensionPrefs(context, "corner_br")),
-                    convertDpToPx(Utils.getDimensionPrefs(context, "corner_br")),
-                    convertDpToPx(Utils.getDimensionPrefs(context, "corner_bl")),
-                    convertDpToPx(Utils.getDimensionPrefs(context, "corner_bl"))
-            });
-        } else {
-            int globalCornerRadius = Utils.getDimensionPrefs(context, "corner_all");
-            drawable.setCornerRadii(new float[]{convertDpToPx(globalCornerRadius), convertDpToPx(globalCornerRadius), convertDpToPx(globalCornerRadius), convertDpToPx(globalCornerRadius), convertDpToPx(globalCornerRadius), convertDpToPx(globalCornerRadius), convertDpToPx(globalCornerRadius), convertDpToPx(globalCornerRadius)});
-        }
-
-        //set button border width and color...
-        int sw = Utils.getDimensionPrefs(context, "stroke_width");
-        drawable.setStroke(convertDpToPx(sw), Color.parseColor(Utils.getColorPrefs(context, "stroke_color")));
-
-        //set button color(s)...
-        drawable.setColor(Color.parseColor("#90CAF9"));
-
-        //set button width and height...
-        int w = Utils.getDimensionPrefs(context, "size_width"), h = Utils.getDimensionPrefs(context, "size_height");
-        btn.setLayoutParams(new LinearLayout.LayoutParams(Utils.getBooleanPrefs(context, "switch_width") ? LinearLayout.LayoutParams.WRAP_CONTENT : convertDpToPx(w), Utils.getBooleanPrefs(context, "switch_height") ? LinearLayout.LayoutParams.WRAP_CONTENT : convertDpToPx(h)));
-
-        btn.setBackground(drawable);
+        applyButtonProperties();
     }
 
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
@@ -104,6 +70,8 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
                     return new Frag3();
                 case 3:
                     return new Frag4();
+                case 4:
+                    return new Frag5();
                 default:
                     return null;
             }
@@ -111,17 +79,56 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
 
         @Override
         public int getCount() {
-            return 4;
+            return 5;
         }
     }
 
     private int convertDpToPx(int dp) {
-        displayMetrics = getResources().getDisplayMetrics();
+        DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
         return (int) ((dp * displayMetrics.density) + .5);
     }
 
     public void onBackPressed() {
         prefs.unregisterOnSharedPreferenceChangeListener(this);
         finishAffinity();
+    }
+
+    private void applyButtonProperties(){
+
+        GradientDrawable drawable = new GradientDrawable();
+
+        //set button corner radii...
+        if (Utils.getBooleanPrefs(ctx, "switch_corner")) {
+            drawable.setCornerRadii(new float[]{
+                    convertDpToPx(Utils.getDimensionPrefs(ctx, "corner_tl")), convertDpToPx(Utils.getDimensionPrefs(ctx, "corner_tl")), convertDpToPx(Utils.getDimensionPrefs(ctx, "corner_tr")), convertDpToPx(Utils.getDimensionPrefs(ctx, "corner_tr")), convertDpToPx(Utils.getDimensionPrefs(ctx, "corner_br")), convertDpToPx(Utils.getDimensionPrefs(ctx, "corner_br")), convertDpToPx(Utils.getDimensionPrefs(ctx, "corner_bl")), convertDpToPx(Utils.getDimensionPrefs(ctx, "corner_bl"))
+            });
+        } else {
+            int globalCornerRadius = Utils.getDimensionPrefs(ctx, "corner_all");
+            drawable.setCornerRadii(new float[]{convertDpToPx(globalCornerRadius), convertDpToPx(globalCornerRadius), convertDpToPx(globalCornerRadius), convertDpToPx(globalCornerRadius), convertDpToPx(globalCornerRadius), convertDpToPx(globalCornerRadius), convertDpToPx(globalCornerRadius), convertDpToPx(globalCornerRadius)});
+        }
+
+        //set button border width and color...
+        int sw = Utils.getDimensionPrefs(ctx, "stroke_width");
+        drawable.setStroke(convertDpToPx(sw), Color.parseColor(Utils.getColorPrefs(ctx, "stroke_color")));
+
+        //set button color(s)...
+        drawable.setColor(Color.parseColor("#90CAF9"));
+
+        //set button width and height...
+        int w = Utils.getDimensionPrefs(ctx, "size_width"), h = Utils.getDimensionPrefs(ctx, "size_height");
+        button.setLayoutParams(new LinearLayout.LayoutParams(Utils.getBooleanPrefs(ctx, "switch_width") ? LinearLayout.LayoutParams.WRAP_CONTENT : convertDpToPx(w), Utils.getBooleanPrefs(ctx, "switch_height") ? LinearLayout.LayoutParams.WRAP_CONTENT : convertDpToPx(h)));
+
+        //set button text...
+        button.setAllCaps(Utils.getBooleanPrefs(ctx, "all_caps"));
+        button.setText(Utils.getColorPrefs(ctx, "button_text"));
+
+        //set button text size in sp...
+        button.setTextSize(TypedValue.COMPLEX_UNIT_SP, Utils.getDimensionPrefs(ctx, "text_size"));
+
+        //set text color..
+        button.setTextColor(Color.parseColor(Utils.getColorPrefs(ctx, "text_color")));
+
+        button.setBackground(drawable);
+
     }
 }
